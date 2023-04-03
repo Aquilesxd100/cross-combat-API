5c033e1d5ae5b63fa748dc75a858f3e8e12d3641
Aquilesxd100 <fernandoalanfillmann@gmail.com>
2023-04-03 17:58:21 -0300

    ajustado pra API serverless do versel

---
+++ api/request.js
@@ -0,0 +1,14 @@
+module.exports = async function (req, res) {
+    if(req.method === "GET") {
+        try {
+            const idAleatorio = Math.trunc(Math.random() * 732);
+            const resposta = await fetch(`https://superheroapi.com/api/2613840595440470/${idAleatorio}`)
+            .then((res) => res.json())
+            .then(data => data);
+            res.json(resposta);
+        }
+        catch(error) {
+            res.json(error)
+        }
+    }
+}
