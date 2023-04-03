0ddf3b1e8f9eaf270b41ca7945deb7e003c8fee8
Aquilesxd100 <fernandoalanfillmann@gmail.com>
2023-04-03 17:10:18 -0300

    inicializacao repositorio

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/cors-disabled', (req, res) => {
+  const idAleatorio = Math.trunc(Math.random() * 732);
+  request(
+    { url: `https://superheroapi.com/api/2613840595440470/${idAleatorio}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
