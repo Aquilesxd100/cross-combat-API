7a5a3363dce6267c5c94d71b57f0a3e0acdbb40e
Aquilesxd100 <fernandoalanfillmann@gmail.com>
2023-04-03 18:47:57 -0300

    teste final

--- api/request.js
+++ api/request.js
@@ -5,7 +5,7 @@ module.exports = async function (req, res) {
             const resposta = await fetch(`https://superheroapi.com/api/2613840595440470/${idAleatorio}`)
             .then((res) => res.json())
             .then(data => data);
-            res.json(resposta);
+            res.json({teste: "teste"});
         }
         catch(error) {
             res.json(error)
