0a166c7f62bbc33233bae1b662f8e23ae2d52e72
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-05-12 10:58:50 -0300

    testes

--- src/index.ts
+++ src/index.ts
@@ -41,12 +41,11 @@ app.post('/gerarHerois/:quantidade',  async (req : Request, res : Response) => {
                     })
                     .catch((error : any) => console.log(error));
 
-/*                 const validationIMGTESTE = await validHeroIMG('https://www.superherodb.com/pictures2/portraits/10/100/1187.jpg'); */
+/*                 const validationIMGTESTE = await validHeroIMG('https://www.superherodb.com/pictures2/portraits/10/100/1187.jpg');
                 
-                const validationIMG = await validHeroIMG(infosHeroi.image.url);
-                
+                const validationIMG = await validHeroIMG(infosHeroi.image.url); */
+                const validationIMG = true;
                 if(infosHeroi && infosHeroi.image.url !== undefined && infosHeroi.image.url !== null && validationIMG) {
-                    console.log(nomeRegistrados)
                     if(!nomeRegistrados.some((nome : string) => nome === infosHeroi.name)) {
                         heroisGerados.push(infosHeroi);
                     }
