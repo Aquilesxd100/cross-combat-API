6e7e77a24bc660a16639aec4557542e98850d55f
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-05-12 11:29:18 -0300

    possivel fix

--- src/helpers/validHeroIMG.ts
+++ src/helpers/validHeroIMG.ts
@@ -1,12 +1,8 @@
 const fetch = require("node-fetch");
 export default async function validHeroIMG(url : string) {
     return await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
+        method: 'GET'
     })
-    .then((res : any) => res.json())
     .then((data : any) => {
         if(data.status < 300) {
             return true;
