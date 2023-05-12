fb7eaba7ed0dba0b47fc91211acb72bbed8a1d92
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-05-12 10:29:50 -0300

    adicao de try e catch em outra funcao

--- src/helpers/validHeroIMG.ts
+++ src/helpers/validHeroIMG.ts
@@ -1,7 +1,12 @@
 export default async function validHeroIMG(url : string) {
-    const checkIMG = await fetch(url)
-    .then((res) => true)
-    .catch((err) => false);
-    console.log(checkIMG)
-    return checkIMG;
+    try {
+        const checkIMG = await fetch(url)
+        .then((res) => true)
+        .catch((err) => false);
+        console.log(checkIMG)
+        return checkIMG;
+    }
+    catch(error) {
+        return false;
+    }
 };
