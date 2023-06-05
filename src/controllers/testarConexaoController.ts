1d3bc28b610c0c133080ddbc081796f441ef48ab
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-06-05 20:13:51 -0300

    configuracao de rota para teste de conexao

--- src/controllers/testarConexaoController.ts
+++ src/controllers/testarConexaoController.ts
@@ -1,13 +1,47 @@
 import { Response, Request } from "express";
+import gerarDisneyUC from "../useCases/gerarDisneyUC";
+import gerarHeroisUC from "../useCases/gerarHeroisUC";
+import gerarAnimesUC from "../useCases/gerarAnimesUC";
 
 export default async function testarConexaoController
 (req: Request, res: Response) {
     try {
-        
+    
+        let cardDisney : any = [];
+        let cardHeroi : any = [];
+        let cardAnimes : any = [];   
 
-        return res.status(200).send({
-            message: "Conectado!"
-        });                    
+
+        const promiseDisney = new Promise(async (resolve, reject) => {
+            cardDisney = await gerarDisneyUC([], 1)
+            resolve(true)
+        });
+
+        const promiseHerois = new Promise(async (resolve, reject) => {
+            cardHeroi = gerarHeroisUC([], 1)
+            resolve(true)
+        });
+
+        const promiseAnimes = new Promise(async (resolve, reject) => {
+            cardAnimes = gerarAnimesUC([], 1)
+            resolve(true)
+        });
+
+
+        await Promise.all([
+            promiseDisney,
+            promiseHerois,
+            promiseAnimes
+        ]);
+
+        if (cardDisney.length && cardHeroi.length && cardAnimes.length) {
+            return res.status(200).send({
+                message: "Conectado!"
+            });   
+        } else {
+            throw new Error("Falhou!");
+        };
+
     } catch(error) {
         console.log(error);
         return res.status(400).send({
