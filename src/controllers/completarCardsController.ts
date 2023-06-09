e56a21f75e7404ff4f0b8a6bb607ddaab2a279ed
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-06-09 15:06:33 -0300

    fix temporario bug

--- src/controllers/completarCardsController.ts
+++ src/controllers/completarCardsController.ts
@@ -17,21 +17,21 @@ export default async function completarCardsController
             const cardHeroi : any = (await gerarHeroisUC(nomesAtuais, 1))[0];
             resposta.cardsHeroi.push(cardHeroi);
             cardsHeroi -= 1;
-            nomesAtuais.push(cardHeroi.name);
+            /* nomesAtuais.push(cardHeroi.name); */
         };
 
         while (cardsDisney) {
             const cardDisney : any = (await gerarDisneyUC(nomesAtuais, 1))[0];
             resposta.cardsDisney.push(cardDisney);
             cardsDisney -= 1;
-            nomesAtuais.push(cardDisney.name);
+            /* nomesAtuais.push(cardDisney.name); */
         };
 
         while (cardsAnime) {
             const cardAnime : any = (await gerarAnimesUC(nomesAtuais, 1))[0];
             resposta.cardsAnime.push(cardAnime);
             cardsAnime -= 1;
-            nomesAtuais.push(cardAnime.canonicalName);
+            /* nomesAtuais.push(cardAnime.canonicalName); */
         };
 
         return res.status(200).send(resposta);
